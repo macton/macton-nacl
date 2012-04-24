70a0abee221760bea299bbc843d9f8a47f93f66c
Mike Acton <macton@gmail.com>
2012-04-24 06:27:48 +0000

    replace nacl_post_message with NaclMessagingPostUtf8

--- hello_01_single_instance/hello.c
+++ hello_01_single_instance/hello.c
@@ -1,8 +1,6 @@
 #include "nacl.h"
 #include <string.h>
 
-void nacl_log_message(const char* msg); // Helper function
-
 // --------------------------------------------------------------------------
 // Required entry point functions -------------------------------------------
 // --------------------------------------------------------------------------
@@ -14,7 +12,7 @@ PP_Bool NaclHandleInputEvent( PP_Resource input_event )
 
 PP_Bool NaclInstanceCreate( uint32_t argc, const char* argn[], const char* argv[] )
 {
-  nacl_log_message("Instance_DidCreate");
+  NaclMessagingPostUtf8("Instance_DidCreate");
   return PP_TRUE;
 }
 
@@ -24,12 +22,12 @@ void NaclInstanceDestroy()
 
 void NaclDidChangeView( PP_Resource view )
 {
-  nacl_log_message("Instance_DidChangeView");
+  NaclMessagingPostUtf8("Instance_DidChangeView");
 }
 
 void  NaclDidChangeFocus( PP_Bool has_focus )
 {
-  nacl_log_message("Instance_DidChangeFocus");
+  NaclMessagingPostUtf8("Instance_DidChangeFocus");
 }
 
 PP_Bool NaclHandleDocumentLoad( PP_Resource url_loader )
@@ -44,21 +42,3 @@ void NaclHandleMessage( PP_Var message )
 void NaclMouseLockLost()
 {
 }
-
-// --------------------------------------------------------------------------
-// Helper functions ---------------------------------------------------------
-// --------------------------------------------------------------------------
-
-// void nacl_log_message(const char* msg)
-// Also see: Yuriy O'Donnell's ArcadeShooter NaCl Postmortem
-//     http://www.kayru.org/articles/arcadeshooter-nacl-postmortem/
-//     http://www.kayru.org/articles/arcadeshooter-nacl-postmortem/index.html.txt
-//     http://www.kayru.org/articles/arcadeshooter-nacl-postmortem/minimal_nacl_gles2.cpp.txt
-
-void nacl_log_message(const char* msg)
-{
-  PP_Var msg_var = NaclVarVarFromUtf8( msg, strlen(msg) );
-  NaclMessagingPostMessage(msg_var);
-  NaclVarRelease(msg_var);
-}
-
